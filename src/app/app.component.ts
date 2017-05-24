fbee97f4e5f359303f73c0ae6a31df182702cc6f
mohamednagy <mohamed.n.haleem@gmail.com>
2017-05-24 09:23:45 +0200

    adding packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotificationsService } from 'angular2-notifications';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  public options = {
+    position: ["bottom", "left"],
+    timeOut: 5000,
+    lastOnBottom: true
+  }
+
+  constructor(private _service: NotificationsService){
+  }
+
+  notify(){
+    this._service.success(
+                          'Some Title',
+                          'Some Content',
+                          {
+                              timeOut: 5000,
+                              showProgressBar: true,
+                              pauseOnHover: false,
+                              clickToClose: false,
+                              maxLength: 10
+                          }
+                      )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, RequestOptions } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+import { SimpleNotificationsModule } from 'angular2-notifications';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {Ng2Webstorage} from 'ngx-webstorage';
 
 import { AppComponent } from './app.component';
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp(new AuthConfig(), http, options);
+}
 
 @NgModule({
   declarations: [
@@ -12,9 +22,19 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    SimpleNotificationsModule.forRoot(),
+    NgbModule.forRoot(),
+    Ng2Webstorage.forRoot({ prefix: '', separator: ''})
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [Http, RequestOptions]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
